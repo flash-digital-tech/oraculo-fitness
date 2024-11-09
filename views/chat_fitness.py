74fdcea28373bf89cca265f9496d66f9311f2a23
Flash Digital Tech <159139704+flash-digital-tech@users.noreply.github.com>
2024-11-09 12:21:02 -0300

    Update chat_fitness.py
    Alteração das variaveis para config()

--- views/chat_fitness.py
+++ views/chat_fitness.py
@@ -11,12 +11,12 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from autentic.authentications import setup_authentication
-
+from decouple import config
 
 
 # --- Verifica se o token da API está nos segredos ---
-if 'REPLICATE_API_TOKEN' in st.secrets:
-    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+if 'REPLICATE_API_TOKEN':
+    replicate_api = config('REPLICATE_API_TOKEN')
 else:
     # Se a chave não está nos segredos, define um valor padrão ou continua sem o token
     replicate_api = None
